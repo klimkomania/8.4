ca83877b8d8c43b398fd6de87f13393d7a87c077
klimkomania <klimkomania@interia.pl>
2017-04-06 13:05:20 +0200

    add files

---
+++ js/scripts.js
@@ -0,0 +1,7 @@
+var name = prompt('Wpisz swoje imię');
+// alert('Witaj, ' + name);
+/*
+console.log('Witaj, ' + name);
+console.log('Witaj, Panie kolego o imieniu' + name);
+*/
+console.log('Witaj, Panie kolego starszy o imieniu ' + name);
